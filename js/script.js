1d75decaa7a4d3b34ae2af63d39cabe014ffa77a
nausheen09 <nausheennadeem849@gamil.com>
2025-09-04 12:49:17 -0700

    task-3

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// ------typing animation-------
+var typed = new Typed(".typing", {
+    strings: ["Graphic Designer", "Web Developer"], 
+    typeSpeed: 80,   
+    backSpeed: 50,   
+    backDelay: 1500, 
+    loop: true
+});
